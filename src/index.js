b6e292a722aebb7b42cf74691d91dad3d5d97f6f
Matt Beasley <matthew.beasley@gmail.com>
2023-03-01 14:05:58 +0000

    CORS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import { Router } from 'itty-router';
+import { Router } from "itty-router";
 
-import Absences from './handlers/absences';
-import Conflict from './handlers/conflicts';
+import Absences from "./handlers/absences";
+import Conflict from "./handlers/conflicts";
 
 const router = Router();
 
 router
-  .get('/api/absences', Absences)
-  .get('/api/conflict/:id', Conflict)
-  .get('*', () => new Response('Not found', { status: 404 }));
+  .get("/api/absences", Absences)
+  .get("/api/conflict/:id", Conflict)
+  .get("*", () => new Response("Not found", { status: 404 }));
 
 export default {
-	async fetch(request, env, ctx) {
-		return router.handle(request)
-	},
+  async fetch(request, env, ctx) {
+    return router.handle(request);
+  },
 };
